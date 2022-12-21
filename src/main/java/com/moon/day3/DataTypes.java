ce3d231f3a42450fef67626ba891732ba7cf5dc3
VamshiVishnu.Kotha1 <vamshivishnu.kotha1@pngaming.com>
2022-12-20 21:07:32 -0600

    Datatype

--- src/main/java/com/moon/day3/DataTypes.java
+++ src/main/java/com/moon/day3/DataTypes.java
@@ -4,8 +4,26 @@ public class DataTypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		boolean flag = true  ;
+		Boolean myBoolean =true ; //null /true /false 
+		
+		char mychar = 'A';
+		
+		//-128 0 127 
+        
+		byte myByte = 55;
+        short myShort =32000;
+        int myint = 200000;
+        long myLong = 213232323;
+        
+        float myFloat = 22.4222F;
+        double myDouble = 22.41;
+        
+        
+		String mystring = "A";
 
-		System.out.println("HEllo World 123");
+		System.out.println(myBoolean);
 	}
 
 }
