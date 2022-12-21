044b02c6b6de7933bc3eabb6c8b5410ec15c5ffa
VamshiVishnu.Kotha1 <vamshivishnu.kotha1@pngaming.com>
2022-12-20 20:25:00 -0600

    sending maven changes

---
+++ src/main/java/com/moon/day3/DataTypes.java
@@ -0,0 +1,11 @@
+package com.moon.day3;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HEllo World");
+	}
+
+}
